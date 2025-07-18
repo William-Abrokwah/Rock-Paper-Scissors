842144fb84850c5f7839a886eb0a3ca980c3a4ac
William Abrokwah <abrokwah27e@gmail.com>
2025-07-18 15:59:46 -0400

    update score display to include ties and adjust game logic for win/loss tracking

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,10 @@
 let computerMove = '';
 let playerMove = '';
 
+let wins = 0;
+let ties = 0;
+let losses = 0;
+
 function chooseComputerMove() {
   let randNum = Math.random();
 
@@ -19,30 +23,39 @@ function playGame() {
   if (playerMove === 'rock') {
     if (computerMove === 'rock') {
       result = 'Tie!';
+      ties++;
     } else if (computerMove === 'paper') {
       result = 'You lose!';
+      losses++;
     } else if (computerMove === 'scissors') {
-      result = 'You win!'; 
+      result = 'You win!';
+      wins++;
     }
   } else if (playerMove === 'paper') {
     if (computerMove === 'rock') {
-      result = 'You win!'; 
+      result = 'You win!';
+      wins++;
     } else if (computerMove === 'paper') {
       result = 'Tie!';
+      ties++;
     } else if (computerMove === 'scissors') {
       result = 'You lose!';
+      losses++;
     }
   } else if (playerMove === 'scissors') {
     if (computerMove === 'rock') {
       result = 'You lose!';
+      losses++;
     } else if (computerMove === 'paper') {
-      result = 'You win!'; 
+      result = 'You win!';
+      wins++;
     } else if (computerMove === 'scissors') {
       result = 'Tie!';
+      ties++;
     }   
   }
 
   document.querySelector('.js-result').innerHTML = `Player chose ${playerMove} and computer chose ${computerMove}. ${result}`;
 
-  document.querySelector('.js-score').innerHTML = `Wins: ${wins} Losses: ${losses}`;
+  document.querySelector('.js-score').innerHTML = `Wins: ${wins} Ties: ${ties} Losses: ${losses}`;
 }
